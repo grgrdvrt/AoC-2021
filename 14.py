f09364fbef712f592bdbcf1ca3711a4ef9b71cb7
grgrdvrt <gdivaret@gmail.com>
2021-12-14 11:25:11 +0100

    fixed

--- 14.py
+++ 14.py
@@ -28,11 +28,8 @@ polymer, pairs = input.strip().split("\n\n")
 pairs = [l.strip().split(" -> ") for l in pairs.strip().split("\n")]
 pairs = {tuple(key):value for key, value in pairs}
 
-print(polymer)
 pairsInPoly = list(zip(polymer, polymer[1:]))
-print(pairsInPoly)
 count = dict(Counter(pairsInPoly))
-print(count)
 for i in range(40):
     newCount = {}
     for (p, n) in count.items():
@@ -47,22 +44,14 @@ for i in range(40):
 
 symbols = {}
 for (p, v) in count.items():
-    symbols.setdefault(p[0], 0)
-    symbols[p[0]] += v
     symbols.setdefault(p[1], 0)
     symbols[p[1]] += v
 
-for c, v in symbols.items():
-    symbols[c] = v//2
-
-print(polymer[0], polymer[-1])
 symbols[polymer[0]] += 1
-symbols[polymer[-1]] += 1
 
 quantities = symbols.values()
 print(max(quantities) - min(quantities))
-# x > 3152788426515
-# actual result is 3152788426516, manually corrected
+
 
 #part1: 8min04
 #total: 1h
